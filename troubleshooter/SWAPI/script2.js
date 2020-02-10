a9d8c0bf6ca131c9f132c25e29825560b325da23
decadentCoder <60242947+decadentCoder@users.noreply.github.com>
2020-02-10 20:47:33 +0100

    Update script2.js

--- troubleshooter/SWAPI/script2.js
+++ troubleshooter/SWAPI/script2.js
@@ -1,82 +1,152 @@
 let logo = document.getElementById("logo");
 let persons = document.getElementById("persons");
 let ships = document.getElementById("ships");
-let display = document.getElementById("tablescreen");
+// let display = document.getElementById("tablescreen");
+let fullDisplay = document.getElementById("present");
+let buttonPrevious = document.getElementById("previous");
+let buttonNext = document.getElementById("next");
 
-let received = []; //where the received data will be stored
+let received = []; //where the received data will be stored //not need
+
+// let url = `https://swapi.co/api`
+let urlStarships = `https://swapi.co/api/starships`  //?=pageNo
+let urlPeople = `https://swapi.co/api/people`
+
+function testing() {
+    alert("testing");
+}
 
 //API request: ships
 
-let requestShips = () =>
-    fetch(`https://swapi.co/api/starships/1/`)
+let requestShips = () => {
+    fetch(urlStarships)
     .then (feedback => feedback.json())  
     .then (feedback => {
         console.log("status feedback before", feedback);
-        console.log("status for received before", received);
-        received = feedback;
-        console.log("status received after merginf with feedback", received);
-        console.log("status feedback", feedback);
-        printShips(display, feedback);
+        console.log("feedback results", feedback.results);
+        // console.log("status for received before", received);
+        
+        // console.log("status received after merginf with feedback", received);
+        // console.log("status feedback", feedback);
+        printShips(fullDisplay, feedback.results);
+        next = feedback.next
+
     });
+
+    buttonNext.addEventListener("click", () => {
+        console.log(next);
+        fetch(next)
+         .then(dataShips => dataShips.json())
+         .then(dataShips => {
+             console.log(dataShips);
+             console.log(dataShips.results);
+             // received = feedback;
+             console.log(dataShips.next);
+             printShips(fullDisplay, dataShips.results);
+             next = dataShips.next
+             })
+         })
+         .finally( () => console.log(next));
     
+    buttonPrevious.addEventListener("click", testing)
+}
 
 let printShips = (table, starships) => {
     table.innerHTML = "";
-    // for (let i of feedback) {
-        table.innerHTML += `<tr>
-            <td>${starships.name}</td>
-            <td>${starships.model}</td>
-            <td>${starships.manufacturer}</td>
-            <td>${starships.cost_in_credits}</td>
-            <td>${starships.crew}</td>
-            <td>${starships.class}</td>
-        ` 
-    // }
+    table.innerHTML += `<thead> <tr>
+        <th> Name </th>
+        <th> Model </th>
+        <th> Manufacturer </th>
+        <th> Cost ( credits ) </th>
+        <th> People Capacity ( crew + passengers ) </th>
+        <th> Class </th>
+        </tr> </thead>`
+    for (let i of starships) {
+    table.innerHTML += 
+        `<tbody> <tr>
+            <td>${i.name}</td>
+            <td>${i.model}</td>
+            <td>${i.manufacturer}</td>
+            <td>${i.cost_in_credits}</td>
+            <td>${i.crew}</td>
+            <td>${i.starship_class}</td>
+        </tr> </tbody>`    
+    }
 }
 
+let next;
+let previous;
 //API request: people
 
-let requestPeople = () => 
-    fetch(`https://swapi.co/api/people/1/`)
+let requestPeople = () => {
+    fetch(urlPeople)
     .then(feedback => feedback.json())
-    .then(json => {
-        console.log(json);
+    .then(feedback => {
+        console.log(feedback);
+        console.log(feedback.results);
         // received = feedback;
-        printPeople(display, json);
-    });
+        console.log(feedback.next);
+        printPeople(fullDisplay, feedback.results);
+        next = feedback.next
+        // buttonNext.addEventListener("click", printPeople(fullDisplay, feedback.next));
+    })
+    .finally( () => console.log(next));
+
+        buttonNext.addEventListener("click", () => {
+           console.log(next);
+           fetch(next)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                console.log(response.results);
+                // received = feedback;
+                console.log(response.next);
+                printPeople(fullDisplay, response.results);
+                next = response.next
+                })
+            })
+            .finally( () => console.log(next));
+
     
+}
 function printPeople(table, people) {
     table.innerHTML = "";
-    // for (const table of received) {
-        table.innerHTML += `<tr>
-            <td>${people.name} </td>
-            <td>${people.height} </td>
-            <td>${people.mass} </td>
-            <td>${people.gender} </td>
-            <td>${people.birth_year} </td>   
-            <td>${people.films} </td>
-            `      
-    // }
+    table.innerHTML +=  `<thead> <tr>
+        <th> Name </th>
+        <th> Height </th>
+        <th> Mass </th>
+        <th> Gender </th>
+        <th> Birth Year </th>
+        <th> Films </th>
+        </tr> </thead>`
+    for (const i of people) {
+        table.innerHTML += `    
+            <tbody> <tr>
+                <td>${i.name} </td>
+                <td>${i.height} </td>
+                <td>${i.mass} </td>
+                <td>${i.gender} </td>
+                <td>${i.birth_year} </td>   
+                <td>${i.films} </td>
+            </td> </tbody>`      
+    }
 }
-
-
-
-function testShips() {
-    alert("ships are clicked");
-}
-
-function testLogo() {
-    alert("logo is clicked");
-}
-
-function testPeople() {
-    alert("people are clicked");
-}
-
-
-
-
 
 ships.addEventListener("click", requestShips);
 persons.addEventListener("click", requestPeople);
-logo.addEventListener("click", testLogo);
+
+
+// fetch(urlStarships) === true ? 
+//     .then(feedback = feedback.json())
+//     .then(feedback => 
+//         console.log(feedback))
+//     //printPeople(feedback.next)
+//     //koja funkcija tuka?!
+
+// )
+
+
+
+// buttonPrevious.addEventListener("click",)
+
+// logo.addEventListener("click", testLogo);
